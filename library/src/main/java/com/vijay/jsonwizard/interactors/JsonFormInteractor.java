3c662b82f3fc3dbf347354d46a46753e5dd2db6b
Juan Jose Rodriguez <jj.rodriguez@lks.es>
2018-12-12 14:05:09 +0100

    Evaluate "show" attribute is present in any widget. If a show expression is present, the widget is shown if the expression returns at least one value

--- library/src/main/java/com/vijay/jsonwizard/interactors/JsonFormInteractor.java
+++ library/src/main/java/com/vijay/jsonwizard/interactors/JsonFormInteractor.java
@@ -1,6 +1,8 @@
 package com.vijay.jsonwizard.interactors;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 
@@ -9,6 +11,8 @@ import com.vijay.jsonwizard.expressions.JsonExpressionResolver;
 import com.vijay.jsonwizard.i18n.JsonFormBundle;
 import com.vijay.jsonwizard.interfaces.CommonListener;
 import com.vijay.jsonwizard.interfaces.FormWidgetFactory;
+import com.vijay.jsonwizard.interfaces.JsonApi;
+import com.vijay.jsonwizard.utils.JsonFormUtils;
 import com.vijay.jsonwizard.widgets.CheckBoxFactory;
 import com.vijay.jsonwizard.widgets.DatePickerFactory;
 import com.vijay.jsonwizard.widgets.EditGroupFactory;
@@ -40,29 +44,68 @@ public class JsonFormInteractor {
     private JsonFormInteractor() {
     }
 
-    public List<View> fetchFormElements(String stepName, Context context, JSONObject parentJson, CommonListener listener, JsonFormBundle bundle,JsonExpressionResolver resolver, int visualizationMode) {
+    public List<View> fetchFormElements(String stepName, Context context, JSONObject parentJson,
+            CommonListener listener, JsonFormBundle bundle, JsonExpressionResolver resolver,
+            int visualizationMode) {
         Log.d(TAG, "fetchFormElements called");
         List<View> viewsFromJson = new ArrayList<>(5);
         try {
             JSONArray fields = parentJson.getJSONArray("fields");
             for (int i = 0; i < fields.length(); i++) {
                 JSONObject childJson = fields.getJSONObject(i);
-                try {
-                    List<View> views = WidgetFactoryRegistry.getWidgetFactory(childJson.getString("type")).
-                            getViewsFromJson(stepName, context, childJson, listener, bundle, resolver, visualizationMode);
-                    if (views.size() > 0) {
-                        viewsFromJson.addAll(views);
+                if (isVisible(childJson, context, resolver)) {
+                    try {
+                        List<View> views = WidgetFactoryRegistry
+                                .getWidgetFactory(childJson.getString("type")).
+                                        getViewsFromJson(stepName, context, childJson, listener,
+                                                bundle,
+                                                resolver, visualizationMode);
+                        if (views.size() > 0) {
+                            viewsFromJson.addAll(views);
+                        }
+                    } catch (Exception e) {
+                        Log.e(TAG,
+                                "Exception occurred in making child view at index : " + i
+                                        + " : Exception is : "
+                                        + e.getMessage(), e);
                     }
-                } catch (Exception e) {
-                    Log.e(TAG,
-                            "Exception occurred in making child view at index : " + i + " : Exception is : "
-                                    + e.getMessage(), e);
                 }
             }
         } catch (JSONException e) {
             Log.e(TAG, "Json exception occurred : " + e.getMessage(), e);
         }
         return viewsFromJson;
+    }
+
+    private boolean isVisible(JSONObject jsonObject, Context context,
+            JsonExpressionResolver resolver) {
+
+        final String showExpression = jsonObject.optString("show");
+        if (TextUtils.isEmpty(showExpression)) {
+            return true;
+        }
+        if (resolver.isValidExpression(showExpression)) {
+            try {
+                JSONObject currentValues = getCurrentValues(context);
+                return resolver.existsExpression(showExpression, currentValues);
+            } catch (JSONException e) {
+                Log.e(TAG, "isVisible: Error evaluating expression " + showExpression, e);
+            }
+            return false;
+        }
+
+        return ("true".equalsIgnoreCase(showExpression));
+    }
+
+    @Nullable
+    private JSONObject getCurrentValues(Context context) throws JSONException {
+        JSONObject currentValues = null;
+        if (context instanceof JsonApi) {
+            String currentJsonState = ((JsonApi) context).currentJsonState();
+            JSONObject currentJsonObject = new JSONObject(currentJsonState);
+            currentValues = JsonFormUtils.extractDataFromForm(currentJsonObject, false);
+        }
+        return currentValues;
     }
 
     public static JsonFormInteractor getInstance() {
