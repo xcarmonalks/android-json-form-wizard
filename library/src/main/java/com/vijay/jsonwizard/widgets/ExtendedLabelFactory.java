45ec0e94b7a137cfaa1db9d6ff950b97d400c48f
Yongliang Zhan <yzhan@indaba.es>
2020-10-06 11:14:19 +0200

    Improved ExtendedLabel widget to support resources via ResourceResolver

--- library/src/main/java/com/vijay/jsonwizard/widgets/ExtendedLabelFactory.java
+++ library/src/main/java/com/vijay/jsonwizard/widgets/ExtendedLabelFactory.java
@@ -1,6 +1,7 @@
 package com.vijay.jsonwizard.widgets;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,13 +38,14 @@ public class ExtendedLabelFactory implements FormWidgetFactory {
 
     @Override
     public List<View> getViewsFromJson(String stepName, Context context, JSONObject jsonObject, CommonListener listener,
-        JsonFormBundle bundle, JsonExpressionResolver resolver, ResourceResolver resourceResolver,
-        int visualizationMode) throws JSONException {
-        return getAsLabel(stepName, context, jsonObject, listener, bundle, resolver, visualizationMode);
+                                       JsonFormBundle bundle, JsonExpressionResolver resolver, ResourceResolver resourceResolver,
+                                       int visualizationMode) throws JSONException {
+        return getAsLabel(stepName, context, jsonObject, listener, bundle, resolver, visualizationMode, resourceResolver);
     }
 
     private List<View> getAsLabel(String stepName, Context context, JSONObject jsonObject, CommonListener listener,
-        JsonFormBundle bundle, JsonExpressionResolver resolver, int visualizationMode) throws JSONException {
+                                  JsonFormBundle bundle, JsonExpressionResolver resolver, int visualizationMode,
+                                  ResourceResolver resourceResolver) throws JSONException {
         List<View> views = new ArrayList<>(1);
 
         HtmlTextView textView = (HtmlTextView) LayoutInflater.from(context).inflate(R.layout.item_extended_label, null);
@@ -82,7 +84,7 @@ public class ExtendedLabelFactory implements FormWidgetFactory {
             }
         }
 
-        textView.setHtml(textValue, new HtmlAssetsImageGetter(textView));
+        textView.setHtml(textValue, new HtmlResourceImageGetter(context, resourceResolver));
         views.add(textView);
         return views;
     }
@@ -107,4 +109,33 @@ public class ExtendedLabelFactory implements FormWidgetFactory {
     }
 
 
+    private class HtmlResourceImageGetter extends HtmlAssetsImageGetter {
+
+        private static final String TAG = "JsonFormsActivity";
+
+        private final Context context;
+        private final ResourceResolver resolver;
+
+        public HtmlResourceImageGetter(Context context, ResourceResolver resolver) {
+            super(context);
+            this.context = context;
+            this.resolver = resolver;
+        }
+
+        @Override
+        public Drawable getDrawable(String source) {
+            Drawable d = super.getDrawable(source);
+            if (d == null) {
+                // Use resource resolver
+                String imagePath = resolver.resolvePath(context, source);
+                d = Drawable.createFromPath(imagePath);
+                if (d == null) {
+                    Log.e(TAG, "source could not be found by resource resolver: " + source);
+                    return null;
+                }
+                d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+            }
+            return d;
+        }
+    }
 }
